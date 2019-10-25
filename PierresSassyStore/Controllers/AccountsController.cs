b2b54cb0bb8fde710077027638225f142048e6fb
Christine Frank <christine.braun13@gmail.com>
2019-10-25 11:10:33 -0700

    Fix user registration error routing and add try catch for registration failures

--- PierresSassyStore/Controllers/AccountsController.cs
+++ PierresSassyStore/Controllers/AccountsController.cs
@@ -2,17 +2,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using PierresSassyStore.Models;
 using System.Threading.Tasks;
+using System;
+using System.Linq;
 using PierresSassyStore.ViewModels;
 
 namespace PierresSassyStore.Controllers
 {
-    public class AccountController : Controller
+    public class AccountsController : Controller
     {
         private readonly PierresSassyStoreContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresSassyStoreContext db)
+        public AccountsController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresSassyStoreContext db)
         {
             _userManager = userManager;
             _signInManager = signInManager;
@@ -26,6 +28,7 @@ namespace PierresSassyStore.Controllers
 
         public ActionResult Register()
         {
+            ViewBag.ErrorMsg = "";
             return View();
         }
 
@@ -33,13 +36,29 @@ namespace PierresSassyStore.Controllers
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
             var user = new ApplicationUser { UserName = model.UserName, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
-            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
+            IdentityResult result;
+            try
             {
+                result = await _userManager.CreateAsync(user, model.Password);
+            }
+            catch(ArgumentNullException)
+            {
+                ViewBag.ErrorMsg = "Please provide content for all values";
+                return View();
+            }
+            if (result.Succeeded)
+            {   
                 return RedirectToAction("Index");
             }
             else
             {
+                var errorList = result.Errors.ToList();
+                string errorMsg = "The following error(s) occurred: ";
+                foreach(var error in errorList)
+                {
+                    errorMsg += error.Code + " " + error.Description + " ";
+                }
+                ViewBag.ErrorMsg = errorMsg;
                 return View();
             }
         }
