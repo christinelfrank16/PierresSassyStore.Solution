7258b8fb3b0b267186ab76c38b5fc69f51faaedd
Christine Frank <christine.braun13@gmail.com>
2019-10-25 08:10:00 -0700

    Initialize project with scaffold using mvc and authentication

---
+++ PierresSassyStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PierresSassyStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
