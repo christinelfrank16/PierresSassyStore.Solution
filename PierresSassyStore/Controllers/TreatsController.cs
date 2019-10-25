e5026c4a17ba4e7d01a74e359d116b250a64e550
Christine Frank <christine.braun13@gmail.com>
2019-10-25 14:54:13 -0700

    Add links for authorized edit and delete. Add model as 'Are you sure' message

--- PierresSassyStore/Controllers/TreatsController.cs
+++ PierresSassyStore/Controllers/TreatsController.cs
@@ -41,11 +41,42 @@ namespace PierresSassyStore.Controllers
 
         [Authorize]
         [HttpPost]
-        public ActionResult Create(Treat treat)
+        public ActionResult Create(Treat treat, int FlavorId)
         {
             _db.Treats.Add(treat);
+            if(FlavorId != 0)
+            {
+                _db.FlavorTreats.Add(new FlavorTreat(){TreatId = treat.TreatId, FlavorId = FlavorId });
+            }
             _db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            Treat model = _db.Treats.FirstOrDefault(t => t.TreatId == id);
+            ViewBag.Flavors = new SelectList(_db.Flavors.ToList(), "FlavorId", "Name");
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Treat treat, int FlavorId)
+        {
+            if (FlavorId != 0)
+            {
+                _db.FlavorTreats.Add(new FlavorTreat() { TreatId = treat.TreatId, FlavorId = FlavorId });
+            }
+            _db.Entry(treat).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Treat model = _db.Treats.Include(t => t.Flavors).ThenInclude(ft => ft.Flavor).FirstOrDefault(t => t.TreatId == id);
+            return View(model);
         }
     }
 }
---
+++ PierresSassyStore/ViewModels/Shared/ModalViewModel.cs
@@ -0,0 +1,10 @@
+namespace PierresSassyStore.ViewModels
+{
+    public class ModalViewModel
+    {
+        public string Name { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public int Id { get; set; }
+    }
+}
