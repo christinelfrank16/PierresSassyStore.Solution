199a3086fae4c95b415a2467357e8147bcdbf8ef
Christine Frank <christine.braun13@gmail.com>
2019-10-25 18:14:18 -0700

    Update card partial view to allow switch between generic treat and flavor images. Attempt to troubleshoot edit errors

--- PierresSassyStore/Controllers/FlavorsController.cs
+++ PierresSassyStore/Controllers/FlavorsController.cs
@@ -31,5 +31,58 @@ namespace PierresSassyStore.Controllers
             List<Flavor> model = _db.Flavors.ToList();
             return View(model);
         }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.TreatIds = new SelectList(_db.Treats.ToList(), "TreatId", "Name");
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Flavor flavor, int TreatIds)
+        {
+            _db.Flavors.Add(flavor);
+            if (TreatIds != 0)
+            {
+                _db.FlavorTreats.Add(new FlavorTreat() { FlavorId = flavor.FlavorId, TreatId = TreatIds });
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            Flavor model = _db.Flavors.FirstOrDefault(t => t.FlavorId == id);
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Flavor flavor)
+        {
+            _db.Entry(flavor).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Flavor thisFlavor = _db.Flavors.Include(f => f.Treats).ThenInclude(ft => ft.Treat).FirstOrDefault(t => t.FlavorId == id);
+            List<Treat> treats = _db.Treats.ToList();
+            return View(new FlavorsDetailsViewModel() { Flavor = thisFlavor, AllTreats = treats });
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Flavor thisFlavor = _db.Flavors.FirstOrDefault(t => t.FlavorId == id);
+            _db.Flavors.Remove(thisFlavor);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
---
+++ PierresSassyStore/Migrations/20191026010253_MinorUpdates.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresSassyStore.Migrations
+{
+    public partial class MinorUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
